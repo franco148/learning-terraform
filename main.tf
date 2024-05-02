a7fed2599840b66056e2532027e6f273ca0ef797
Franco Arratia <franck_ral@hotmail.com>
2024-05-01 23:17:33 -0400

    learning-terraform: Modules - Configuring security groups by modules.

--- main.tf
+++ main.tf
@@ -21,52 +21,67 @@ data "aws_vpc" "default" {
 resource "aws_instance" "tf_learning" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  
-  vpc_security_group_ids = [ aws_security_group.tf_learning_sg.id ]
+
+  # vpc_security_group_ids = [ aws_security_group.tf_learning_sg.id ] // Before security group
+  vpc_security_group_ids = [ module.tf-security-group-module.security_group_id ]
 
   tags = {
     Name = "learning-terraform"
   }
 }
 
-resource "aws_security_group" "tf_learning_sg" {
-  name        = "tf_learning_sg"
-  description = "Allow http and https in. Allow everything out"
+module "tf-security-group-module" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.2"
+  name    = "tf_learning_sg_2"
 
-  tags = {
-    Terraform = "true"
-  }
+  vpc_id  = data.aws_vpc.default.id
 
-  vpc_id = data.aws_vpc.default.id
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "tf_learning_sg_rule_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+# resource "aws_security_group" "tf_learning_sg" {
+#   name        = "tf_learning_sg"
+#   description = "Allow http and https in. Allow everything out"
 
-  security_group_id = aws_security_group.tf_learning_sg.id
-}
+#   tags = {
+#     Terraform = "true"
+#   }
+
+#   vpc_id = data.aws_vpc.default.id
+# }
+
+# resource "aws_security_group_rule" "tf_learning_sg_rule_http_in" {
+#   type        = "ingress"
+#   from_port   = 80
+#   to_port     = 80
+#   protocol    = "tcp"
+#   cidr_blocks = ["0.0.0.0/0"]
+
+#   security_group_id = aws_security_group.tf_learning_sg.id
+# }
 
 
-resource "aws_security_group_rule" "tf_learning_sg_rule_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+# resource "aws_security_group_rule" "tf_learning_sg_rule_https_in" {
+#   type        = "ingress"
+#   from_port   = 443
+#   to_port     = 443
+#   protocol    = "tcp"
+#   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.tf_learning_sg.id
-}
+#   security_group_id = aws_security_group.tf_learning_sg.id
+# }
 
 
-resource "aws_security_group_rule" "tf_learning_sg_rule_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.tf_learning_sg.id
-}
+# resource "aws_security_group_rule" "tf_learning_sg_rule_everything_out" {
+#   type        = "egress"
+#   from_port   = 0
+#   to_port     = 0
+#   protocol    = "-1"
+#   cidr_blocks = ["0.0.0.0/0"]
+#   security_group_id = aws_security_group.tf_learning_sg.id
+# }
