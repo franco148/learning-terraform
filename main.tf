e0a451ca75c449e105fee98789f79db25cd53447
Franco Arratia <franck_ral@hotmail.com>
2024-05-01 23:46:47 -0400

    learning-terraform: Modules - Ready to scale

--- main.tf
+++ main.tf
@@ -18,12 +18,32 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "tf_vpc_module" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "tf_learning_vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["sa-east-1a", "sa-east-1b", "sa-east-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "tf_learning" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
   # vpc_security_group_ids = [ aws_security_group.tf_learning_sg.id ] // Before security group
   vpc_security_group_ids = [ module.tf-security-group-module.security_group_id ]
+
+  subnet_id = module.tf_vpc_module.public_subnets[0]
 
   tags = {
     Name = "learning-terraform"
@@ -35,7 +55,8 @@ module "tf-security-group-module" {
   version = "5.1.2"
   name    = "tf_learning_sg_2"
 
-  vpc_id  = data.aws_vpc.default.id
+  # vpc_id  = data.aws_vpc.default.id // Before vpc module
+  vpc_id = module.tf_vpc_module.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
